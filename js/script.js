86e39b105d83d19d81468e58fa674a550b2a3633
sueselle <sue@thehappyuser.com>
2023-11-27 12:45:25 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var fans = document.querySelector(".fans");
+//console.log(fans);
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+
+fans.innerText = "123K";
+fish.innerText = "47K";
+pets.innerText = "20K";
